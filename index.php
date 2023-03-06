e6e2988cfb0b4497dce8ee525901952e007b1d0f
Khalid Zia Rabbani <88278471+khalidziarabbani@users.noreply.github.com>
2023-03-06 16:06:04 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<style>
+        body {
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: black;
+        }
+        canvas {
+            border: 1px solid red;
+            
+        }
+</style>
+
+<div style="position: relative;">
+    <p style="position: absolute; z-index: 10; color: white; 
+            margin: 0; margin-left: 10px; margin-top: 10px; 
+                font-family: sans-serif; font-size: 14px;">
+        <span> Score: </span> <span id="scoreElement">0</span>
+    </p>
+    <canvas id="canvas"></canvas>
+</div>
+
+<script src="index.js"></script>
